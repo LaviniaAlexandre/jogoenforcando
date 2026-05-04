e6679441d59340f20a917b1fcf1b4c16d620e532
aluno <lavinia.alexandre@alunos.ifsuldeminas.edu.br>
2026-05-04 11:32:44 -0300

    04/05/2026

--- app/src/main/java/com/example/enforcando/TelaJogo.java
+++ app/src/main/java/com/example/enforcando/TelaJogo.java
@@ -1,6 +1,9 @@
 package com.example.enforcando;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.ImageView;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -8,17 +11,53 @@ import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
 
-public class TelaJogo extends AppCompatActivity {
+import java.util.ArrayList;
+
+public class TelaJogo extends AppCompatActivity implements View.OnClickListener{
+    private ImageView imagem;
+
+    private ArrayList<Integer> listaimagens;
+    private int indiceListaImagem;
+
+    private Button b1;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         EdgeToEdge.enable(this);
         setContentView(R.layout.activity_tela_jogo);
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.id08), (v, insets) -> {
             Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
             return insets;
         });
+        imagem = findViewById(R.id.imageView2);
+        indiceListaImagem = -1;
+        listaimagens = new ArrayList<Integer>();
+        listaimagens.add(R.drawable.forca_1_9);
+        listaimagens.add(R.drawable.forca_2_9);
+        listaimagens.add(R.drawable.forca_3_9);
+        listaimagens.add(R.drawable.forca_4_9);
+        listaimagens.add(R.drawable.forca_5_9);
+        listaimagens.add(R.drawable.forca_6_9);
+        listaimagens.add(R.drawable.forca_7_9);
+        listaimagens.add(R.drawable.forca_8_9);
+        listaimagens.add(R.drawable.forca_10_9);
+        listaimagens.add(R.drawable.forca_11_9);
+
+        b1 = findViewById((R.id.id01));
+        b1 = setOnClickListener(this);
+
+    }
+
+    public void atualizaForca(){
+       indiceListaImagem++;
+        imagem.setImageResource(listaimagens.get(indiceListaImagem));
+    }
+
+    @Override
+    public void onClick(View v) {
+        atualizaForca();
+
     }
 }
